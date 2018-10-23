840793ac1a7f182ae0abd8b5530d6c95b4da3ef3
ArseniyKolosha <35842248+ArseniyKolosha@users.noreply.github.com>
2018-10-23 21:26:26 +0300

    App optimization

--- NewsApp/Model/Model.swift
+++ NewsApp/Model/Model.swift
@@ -9,18 +9,21 @@
 import Foundation
 
 //https://newsapi.org/v2/everything?q=bitcoin&from=2018-09-23&sortBy=publishedAt&apiKey=354d804260494f41a9bc8e180647219c
-
+var  urlToData: URL {
+    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0] + "/data.json"
+    let urlPath = URL(fileURLWithPath: path)
+    return urlPath
+}
 var articles: [Article] = []
 func loadNews() {
     let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2018-09-23&sortBy=publishedAt&apiKey=354d804260494f41a9bc8e180647219c")
     let session = URLSession(configuration: .default)
     let downloadTask = session.downloadTask(with: url!) { (urlFile , responce, error) in
         if urlFile != nil {
-           let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0] + "/data.json"
-            let urlPath = URL(fileURLWithPath: path)
-           try? FileManager.default.copyItem(at:  urlFile!, to: urlPath)
+          
+           try? FileManager.default.copyItem(at:  urlFile!, to: urlToData)
             parseNews()
-            print(urlPath)
+            print(urlToData)
             print(articles.count)
         }
     }
@@ -28,16 +31,29 @@ func loadNews() {
 }
 
 func parseNews() {
-    let path = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0] + "/data.json"
-    let urlPath = URL(fileURLWithPath: path)
+
     
-    let data = try? Data(contentsOf: urlPath)
-    let rootDictionary = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
-    let array = rootDictionary!["articles"] as! [Dictionary<String, Any>]
-    var returnArray: [Article] = []
-    for dict in array {
-         let  newArticle = Article(dictionary: dict)
-        returnArray.append(newArticle)
+    let data = try? Data(contentsOf: urlToData)
+    if data == nil {
+        return
     }
-    articles = returnArray
+    let rootDictionaryAny = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! Dictionary<String, Any>
+    if rootDictionaryAny == nil {
+        return
+    }
+    
+    let rootDictionary = rootDictionaryAny as? Dictionary<String, Any>
+    if rootDictionary == nil {
+        return
+    }
+    
+    if let array = rootDictionary!["articles"] as? [Dictionary<String, Any>]{
+        var returnArray: [Article] = []
+        for dict in array {
+            let  newArticle = Article(dictionary: dict)
+            returnArray.append(newArticle)
+        }
+        articles = returnArray
+    }
+   
 }
