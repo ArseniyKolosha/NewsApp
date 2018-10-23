e1c5b4f86e0adfdc88e575fd47a1873a09957e4d
ArseniyKolosha <35842248+ArseniyKolosha@users.noreply.github.com>
2018-10-24 00:17:56 +0300

    Load image + activityIndicator

--- NewsApp/OneNewsViewController.swift
+++ NewsApp/OneNewsViewController.swift
@@ -12,14 +12,25 @@ class OneNewsViewController: UIViewController {
 
     var article: Article!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        titleLabel.text = article.title
        descriptionLabel.text = article.description
+        
+        DispatchQueue.main.async {
+            if let url = URL(string: self.article.urlToImage) {
+                if let data = try? Data(contentsOf: url) {
+                   self.imageView.image = UIImage(data: data)
+                }
+                self.activityIndicator.isHidden = true
+            }
+        }
         
     }
     
